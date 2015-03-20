24cdee729323cde83206f93b27eeb1034373f5af
pacman9269 <swarwick@sfu.ca>
2015-03-19 19:22:45 -0700

    setup multiple page views

--- src/app/index.js
+++ src/app/index.js
@@ -7,6 +7,18 @@ angular.module('iat381Final', ['ngAnimate', 'ngTouch', 'ngRoute'])
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl'
       })
+
+      .when('/globe', {
+        templateUrl: 'app/main/globe.html',
+        controller: 'MainCtrl'
+      })
+
+       .when('/history', {
+        templateUrl: 'app/main/history.html',
+        controller: 'MainCtrl'
+      })
+
+
       .otherwise({
         redirectTo: '/'
       });
