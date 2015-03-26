c9e0130ce6bc9b405869e8370538a22b8082abcb
ivr56 <ivor.sagala@gmail.com>
2015-03-26 15:13:45 -0700

    "Updates to JSON"

---
+++ globe_module/js/currency.js
@@ -0,0 +1,25 @@
+// //Convert Country Code to Currency Code
+// currencycode = '[{
+//   "CA" : "CAD",
+//   "US" : "USS"
+// }]';
+//
+// //Convert Country Code
+// countrycode = '[{
+// "CA" : "Canada",
+// "US" : "United States"
+// }]';
+//
+// function convertcodes()
+// {
+//   //Parse Country ISO Code
+//   var isocode = JSON.parse(countrycode);
+//   //Parse Country Currency Code
+//   var covertcode = JSON.parse(currencycode);
+//   //Print Statements
+//   console.log("ISO Code: " + isocode[0].countrysel_buffer);
+//   var countrycode_buffer  = isocode[0].countrysel_buffer;
+//   //Print Currency Statement
+//   console.log("Currency Code: " + convertcode[0].countrycode_buffer);
+//
+// }
