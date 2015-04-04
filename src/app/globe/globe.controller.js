dcb1285f024a935df6e1eb24f048a5ca5ec182ea
ivr56 <ivor.sagala@gmail.com>
2015-04-04 00:36:03 -0700

    Fixes

--- src/app/globe/globe.controller.js
+++
@@ -1,3 +0,0 @@
-angular.module('iat381Final')
-  .controller('GlobeCtrl', function ($scope) {
-  });
--- src/app/index.js
+++ src/app/index.js
@@ -24,47 +24,47 @@ angular.module('iat381Final', ['ngAnimate', 'ngTouch', 'ngRoute'])
       });
   })
 ;
-
-
-function historyCtrl($scope) {
-
-  $scope.histories = [];
-
-
-  $scope.addhistory = function () {
-    $scope.histories.push({text:$scope.formTodoText, done:false});
-    $scope.formTodoText = '';
-  };
-
-
-}
-
-angular.module('scopeExample', [])
-.controller('GreetController', ['$scope', '$rootScope', function($scope, $rootScope) {
-  $scope.name = 'World';
-  $rootScope.department = 'Angular';
-}])
-.controller('ListController', ['$scope', function($scope) {
-  $scope.names = ['Igor', 'Misko', 'Vojta'];
-}]);
-
-
-
-angular.module('toDo',[])
-
-.service('dataService', function() {
-  // private variable
-  var _dataObj = {};
-
-  this.dataObj = _dataObj;
-})
-
-.controller('One', function($scope, dataService) {
-  $scope.data = dataService.dataObj;
-})
-
-.controller('Two', function($scope, dataService) {
-  $scope.data = dataService.dataObj;
-})
-
-;
+// 
+//
+// function historyCtrl($scope) {
+//
+//   $scope.histories = [];
+//
+//
+//   $scope.addhistory = function () {
+//     $scope.histories.push({text:$scope.formTodoText, done:false});
+//     $scope.formTodoText = '';
+//   };
+//
+//
+// }
+//
+// angular.module('scopeExample', [])
+// .controller('GreetController', ['$scope', '$rootScope', function($scope, $rootScope) {
+//   $scope.name = 'World';
+//   $rootScope.department = 'Angular';
+// }])
+// .controller('ListController', ['$scope', function($scope) {
+//   $scope.names = ['Igor', 'Misko', 'Vojta'];
+// }]);
+//
+//
+//
+// angular.module('toDo',[])
+//
+// .service('dataService', function() {
+//   // private variable
+//   var _dataObj = {};
+//
+//   this.dataObj = _dataObj;
+// })
+//
+// .controller('One', function($scope, dataService) {
+//   $scope.data = dataService.dataObj;
+// })
+//
+// .controller('Two', function($scope, dataService) {
+//   $scope.data = dataService.dataObj;
+// })
+//
+// ;
