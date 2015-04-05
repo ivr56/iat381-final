6620febfb1d9906670ee37af9bbdf47f3eac00e9
irenew <nxwang@sfu.ca>
2015-04-04 22:50:09 -0700

    finished 4 different value storage for sklad-just need styling

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,7 @@ sklad.open(dbName, {
   migration: {
     '1': function (database) {
       var objStore = database.createObjectStore('todos', {autoIncrement: true});
-      objStore.createIndex('toCountry_search', 'toCountryName', {unique: false});
+      objStore.createIndex('description_search', 'description', {unique: false});
       objStore.createIndex('inputAmount_search', 'inputAmount', {unique: false});
       objStore.createIndex('fromCountryName_search', 'fromCountryName', {unique: false});
       objStore.createIndex('outcomeAmount_search', 'outcomeAmount', {unique: false});
@@ -17,10 +17,10 @@ sklad.open(dbName, {
         autoIncrement: true,
         keyPath: 'timestamp'
       });
-      objStore.createIndex('toCountry_search', 'toCountryName', {unique: false})
-      objStore.createIndex('inputAmount_search', 'inputAmount', {unique: false})
-      objStore.createIndex('fromCountryName_search', 'fromCountryName', {unique: false})
-      objStore.createIndex('outcomeAmount_search', 'outcomeAmount', {unique: false})
+      objStore.createIndex('description_search', 'description', {unique: false})
+      // objStore.createIndex('inputAmount_search', 'inputAmount', {unique: false})
+      // objStore.createIndex('fromCountryName_search', 'fromCountryName', {unique: false})
+      // objStore.createIndex('outcomeAmount_search', 'outcomeAmount', {unique: false})
       objStore.createIndex('done', 'done', {unique: false});
     }
   }
@@ -30,7 +30,7 @@ function (err, conn) {
 
   if (err) { throw err; }
   $(function () {
-    var $toCountryName = $('#toCountryName');
+    var $description = $('#description');
     var $inputAmount = $('#inputAmount');
     var $fromCountryName = $('#fromCountryName');
     var $outcomeAmount = $('#outcomeAmount');
@@ -40,7 +40,7 @@ function (err, conn) {
     function updateRows(conn) {
       conn
         .get({
-          todos: {toCountryName: sklad.DESC, index: 'toCountryName_search'}
+          todos: {description: sklad.DESC, index: 'description_search'}
         }, function (err, data) {
           if (err) { return console.error(err); }
 
@@ -54,21 +54,21 @@ function (err, conn) {
             var $time = $(document.createElement('p'));
             // var $l = $(document.createElement('h2'));
             //if item is done
-            if (todo.value.done) {
-              $li.css({'display': 'none'})
-            }
-            $firstCountry.text(todo.value.toCountryName);
+            // if (todo.value.done) {
+            //   $li.css({'display': 'none'})
+            // }
+            $firstCountry.text(todo.value.description);
             $firstAmount.text(todo.value.inputAmount);
             $secCountry.text(todo.value.fromCountryName);
             $secAmount.text(todo.value.outcomeAmount);
             $time.text(todo.value.timestamp);
-            $li.click (function () {
-              todo.value.done = true;
-              conn.upsert('todos', todo.value, function (err){
-                if (err) {return console.error();}
-                updateRows(conn);
-              })
-            });
+            // $li.click (function () {
+            //   todo.value.done = true;
+            //   conn.upsert('todos', todo.value, function (err){
+            //     if (err) {return console.error();}
+            //     updateRows(conn);
+            //   })
+            // });
             $list.append($time);
             $list.append($firstCountry);
             $list.append($secCountry);
@@ -81,14 +81,14 @@ function (err, conn) {
     updateRows(conn);
 
     $add.click(function () {
-      if (!$toCountryName.val().trim()) {
+      if (!$description.val().trim()) {
         return;
       }
       conn.insert({
         todos: [
           {
             timestamp: Date.now(),
-            toCountryName: $toCountryName.val().trim(),
+            description: $description.val().trim(),
             inputAmount: $inputAmount.val().trim(),
             fromCountryName: $fromCountryName.val().trim(),
             outcomeAmount: $outcomeAmount.val().trim(),
